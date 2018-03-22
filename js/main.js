3e7b0e710db3387b0cec7cb15ff7ac36a3ae1be5
RamizIb <RamizIb@yandex.ru>
2018-03-22 12:58:30 +0300

    Add mobile menu, 404.html

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ $("a[href='#top']").click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
+
+$(".menu-toggle").click(function(){
+  $(".navbar__ul").toggleClass("navbar__ul-open");
+});
